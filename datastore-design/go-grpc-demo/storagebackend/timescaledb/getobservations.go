0866bafccc1778149aad9b069506df02a5eb7e45
Jo Asplin <jo.asplin@met.no>
2023-07-10 07:53:29 +0200

    Implemented GetObservations

--- datastore-design/go-grpc-demo/storagebackend/timescaledb/getobservations.go
+++ datastore-design/go-grpc-demo/storagebackend/timescaledb/getobservations.go
@@ -1,56 +1,68 @@
 package timescaledb
 
 import (
+	"database/sql"
 	"datastore/datastore"
 	"fmt"
 
 	_ "github.com/lib/pq"
 )
 
+// retrieveObsForTS retrieves into obs observations for time series tsID in
+// open-ended range [fromTime, toTime>.
+// Returns nil upon success, otherwise error.
+func retrieveObsForTS(
+	db *sql.DB, tsID int64, fromTime, toTime int64, obs *[]*datastore.Observation) error {
+
+	rows, err := db.Query(fmt.Sprintf(`
+		SELECT EXTRACT(EPOCH FROM tstamp)::INT, value, field1, field2 FROM observations
+		WHERE ts_id = %d
+		AND tstamp >= to_timestamp(%d)
+		AND tstamp <  to_timestamp(%d)
+		ORDER BY tstamp ASC
+	`, tsID, fromTime, toTime))
+	if err != nil {
+		return fmt.Errorf("db.Query() failed: %v", err)
+	}
+	for rows.Next() {
+		var (
+			obsTime int64
+			obsVal float64
+			field1, field2 string
+		)
+		if err := rows.Scan(&obsTime, &obsVal, &field1, &field2); err != nil {
+			return fmt.Errorf("rows.Scan() failed: %v", err)
+		}
+		(*obs) = append(*obs, &datastore.Observation{
+			Time: obsTime,
+			Value: obsVal,
+			Metadata: &datastore.ObsMetadata{
+				Field1: field1,
+				Field2: field2,
+			},
+		})
+	}
+
+	return nil
+}
+
 // GetObservations ... (see documentation in StorageBackend interface)
 func (sbe *TimescaleDB) GetObservations(request *datastore.GetObsRequest) (
 	*datastore.GetObsResponse, error) {
 
-	// TODO
+	// TODO: validate request.Tsids (ensure it doesn't contains duplicates etc.)
+	tsObs := make([]*datastore.TSObservations, len(request.Tsids))
+	for i, tsID := range request.Tsids {
+		obs := []*datastore.Observation{}
+		if err := retrieveObsForTS(
+			sbe.Db, tsID, request.Fromtime, request.Totime, &obs); err != nil {
+			return nil, fmt.Errorf("retrieveObsForTS() failed (i: %d, tsID: %d): %v", i, tsID, err)
+		}
+		tsObs[i] = &datastore.TSObservations{
+			Tsid: tsID,
+			Obs: obs,
+		}
+	}
 
-	// tsobs := []*datastore.TSObservations{}
-
-	// // --- BEGIN TODO: retrieve observations from database -----------
-	// // ... for now:
-	// var nSteps int64 = 3
-	// max := func(x, y int64) int64 {
-	// 	if x > y {
-	// 		return x
-	// 	}
-	// 	return y
-	// }
-	// timeStep := max(1, (request.Totime - request.Fromtime) / nSteps)
-	// for _, tsid := range request.Tsids {
-	// 	obs := []*datastore.Observation{}
-	// 	obsTime := request.Fromtime
-	// 	for i := 0; obsTime <= request.Totime; i++ {
-	// 		obsVal := 10 + float64(i)
-	// 		obs = append(obs, &datastore.Observation{
-	// 			Time: obsTime,
-	// 			Value: obsVal,
-	// 			Metadata: &datastore.ObsMetadata{
-	// 				Field1: fmt.Sprintf("value1 (%d)", i),
-	// 				Field2: fmt.Sprintf("value2 (%d)", i),
-	// 			},
-	// 		})
-	// 		obsTime += timeStep
-	// 	}
-	// 	tsobs = append(tsobs, &datastore.TSObservations{
-	// 		Tsid: tsid,
-	// 		Obs: obs,
-	// 	})
-	// }
-	// // --- END TODO: retrieve observations from storage backend -----------
-
-	// return &datastore.GetObsResponse{
-	// 	Status: -1, // for now
-	// 	Tsobs: tsobs,
-	// }, nil
-
-	return nil, fmt.Errorf("TimescaleDB/GetObservations() not implemented yet")
+	return &datastore.GetObsResponse{Tsobs: tsObs}, nil
 }
