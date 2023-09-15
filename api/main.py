5d7db7cebe0d21b4ea49292df036e99a2b1d5a45
Lukas Phaf <lukas.phaf@knmi.nl>
2023-09-15 16:01:25 +0200

    Deal with multiple parameters (and stations).

--- api/main.py
+++ api/main.py
@@ -4,7 +4,7 @@
 import os
 from datetime import datetime
 from datetime import timezone
-from typing import Annotated
+from itertools import groupby
 
 from brotli_asgi import BrotliMiddleware
 
@@ -19,38 +19,43 @@ import datastore_pb2 as dstore
 import datastore_pb2_grpc as dstore_grpc
 import grpc
 
-from covjson_pydantic.coverage import Coverage
+from covjson_pydantic.coverage import Coverage, CoverageCollection
 from covjson_pydantic.domain import Domain, DomainType, Axes, ValuesAxis
 
 app = FastAPI()
 app.add_middleware(BrotliMiddleware)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def collect_data(time_serie, observations):
+    lat = time_serie.metadata.pos.lat
+    lon = time_serie.metadata.pos.lon
+    tuples = ((o.time.ToDatetime().replace(tzinfo=timezone.utc), o.value) for o in observations.obs)
+    (times, values) = zip(*tuples)
+    param_id = time_serie.metadata.param_id
+
+    return (lat, lon, times), param_id, values
 
 
 @app.get(
     "/collections/observations/locations/{location_id}",
-    response_model=Coverage,
-    response_model_exclude_none=True,)
+    response_model=Coverage | CoverageCollection,
+    response_model_exclude_none=True, )
 def read_item(location_id: str, parameter_name: str = Query(..., alias="parameter-name")):
     # TODO: There is no error handling of any kind at the moment! This is just a quick and dirty demo
+    # TODO: Code does not handle nan when serialising to JSON
     with grpc.insecure_channel(f"{os.getenv('DSHOST', 'localhost')}:{os.getenv('DSPORT', '50050')}") as channel:
         grpc_stub = dstore_grpc.DatastoreStub(channel)
         ts_request = dstore.FindTSRequest(
+            # station_ids=[location_id] + ['06240'],  # Hack to test CoverageCollection logic
             station_ids=[location_id],
             param_ids=list(map(str.strip, parameter_name.split(",")))
         )
         ts_response = grpc_stub.FindTimeSeries(ts_request)
-        assert len(ts_response.tseries) == 1
-        # ts_id = ts_response.tseries[0].id
 
         from_time = Timestamp()
-        from_time.FromDatetime(datetime(2022, 12, 31))   # TODO: Get from request
+        from_time.FromDatetime(datetime(2022, 12, 31))  # TODO: Get from request
         to_time = Timestamp()
-        to_time.FromDatetime(datetime(2023, 11, 1))      # TODO: Get from request
+        to_time.FromDatetime(datetime(2023, 11, 1))  # TODO: Get from request
         request = dstore.GetObsRequest(
             tsids=[ts.id for ts in ts_response.tseries],
             fromtime=from_time,
@@ -58,21 +63,27 @@ def read_item(location_id: str, parameter_name: str = Query(..., alias="paramete
         )
         response = grpc_stub.GetObservations(request)
 
-        assert len(response.tsobs) == 1
-        assert response.tsobs[0].tsid == ts_response.tseries[0].id
+        for i in range(0, len(ts_response.tseries)):
+            assert response.tsobs[i].tsid == ts_response.tseries[i].id
 
         # Collect data
-        lat = ts_response.tseries[0].metadata.pos.lat
-        lon = ts_response.tseries[0].metadata.pos.lon
-        tuples = ((o.time.ToDatetime().replace(tzinfo=timezone.utc), o.value) for o in response.tsobs[0].obs)
-        (times, values) = zip(*tuples)
+        coverages = []
+        data = [collect_data(time_serie, observations) for (time_serie, observations) in zip(ts_response.tseries, response.tsobs)]
 
-        domain = Domain(domainType=DomainType.point_series,
-                        axes=Axes(x=ValuesAxis[float](values=[lon]),
-                                  y=ValuesAxis[float](values=[lat]),
-                                  t=ValuesAxis[AwareDatetime](values=times)))
-        nd_array = NdArray(values=values, axisNames=["t", "y", "x"], shape=[len(values), 1, 1])
+        # Need to sort before using groupBy
+        data.sort(key=lambda x: x[0])
+        # The multiple coverage logic is not needed for this endpoint, but we want to share this code between endpoints
+        for (lat, lon, times), group in groupby(data, lambda x: x[0]):
+            domain = Domain(domainType=DomainType.point_series,
+                            axes=Axes(x=ValuesAxis[float](values=[lon]),
+                                      y=ValuesAxis[float](values=[lat]),
+                                      t=ValuesAxis[AwareDatetime](values=times)))
+            ranges = {param_id: NdArray(values=values, axisNames=["t", "y", "x"], shape=[len(values), 1, 1])
+                      for ((_, _, _), param_id, values) in group}
 
-        cov = Coverage(domain=domain, ranges={"rh": nd_array})  # TODO: Not hardcoded!
+            coverages.append(Coverage(domain=domain, ranges=ranges))
 
-    return cov
+        if len(coverages) == 1:
+            return coverages[0]
+        else:
+            return CoverageCollection(coverages=coverages)
