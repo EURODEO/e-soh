eb95951db0e06553638aa897eb4097c8fcbe8621
Lukas Phaf <lukas.phaf@knmi.nl>
2023-09-15 15:06:41 +0200

    Streamline code.

--- api/main.py
+++ api/main.py
@@ -1,14 +1,16 @@
 # Run with:
-# For developing:    uvicorn main:app --reload
+# For developing:    uvicorn main:app --reload --bind=0.0.0.0:8000
 
 import os
 from datetime import datetime
 from datetime import timezone
+from typing import Annotated
 
 from brotli_asgi import BrotliMiddleware
 
 from covjson_pydantic.ndarray import NdArray
 from fastapi import FastAPI
+from fastapi import Query
 
 from google.protobuf.timestamp_pb2 import Timestamp
 from pydantic import AwareDatetime
@@ -33,41 +35,37 @@ def read_root():
     "/collections/observations/locations/{location_id}",
     response_model=Coverage,
     response_model_exclude_none=True,)
-def read_item(location_id: str):
+def read_item(location_id: str, parameter_name: str = Query(..., alias="parameter-name")):
     # TODO: There is no error handling of any kind at the moment! This is just a quick and dirty demo
     with grpc.insecure_channel(f"{os.getenv('DSHOST', 'localhost')}:{os.getenv('DSPORT', '50050')}") as channel:
         grpc_stub = dstore_grpc.DatastoreStub(channel)
         ts_request = dstore.FindTSRequest(
             station_ids=[location_id],
-            param_ids=["rh"]   # TODO: Get from request
+            param_ids=list(map(str.strip, parameter_name.split(",")))
         )
         ts_response = grpc_stub.FindTimeSeries(ts_request)
         assert len(ts_response.tseries) == 1
-        ts_id = ts_response.tseries[0].id
+        # ts_id = ts_response.tseries[0].id
 
         from_time = Timestamp()
         from_time.FromDatetime(datetime(2022, 12, 31))   # TODO: Get from request
         to_time = Timestamp()
         to_time.FromDatetime(datetime(2023, 11, 1))      # TODO: Get from request
         request = dstore.GetObsRequest(
-            tsids=[ts_id],
+            tsids=[ts.id for ts in ts_response.tseries],
             fromtime=from_time,
             totime=to_time,
         )
         response = grpc_stub.GetObservations(request)
 
         assert len(response.tsobs) == 1
-        assert response.tsobs[0].tsid == ts_id
+        assert response.tsobs[0].tsid == ts_response.tseries[0].id
 
         # Collect data
         lat = ts_response.tseries[0].metadata.pos.lat
         lon = ts_response.tseries[0].metadata.pos.lon
-        times = []
-        values = []
-        for o in response.tsobs[0].obs:
-            dt = o.time.ToDatetime().replace(tzinfo=timezone.utc)
-            times.append(dt)
-            values.append(o.value)
+        tuples = ((o.time.ToDatetime().replace(tzinfo=timezone.utc), o.value) for o in response.tsobs[0].obs)
+        (times, values) = zip(*tuples)
 
         domain = Domain(domainType=DomainType.point_series,
                         axes=Axes(x=ValuesAxis[float](values=[lon]),
