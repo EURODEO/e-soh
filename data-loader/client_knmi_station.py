165e2809691c6ddb84df5e7bc31413581b646417
Lukas Phaf <lukas.phaf@knmi.nl>
2023-09-27 11:54:25 +0200

    Restore path.

--- data-loader/client_knmi_station.py
+++ data-loader/client_knmi_station.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
 
     print("Starting with creating the time series and observations requests.")
     create_requests_start = perf_counter()
-    file_path = Path(Path(__file__).parents[1] / "test-data" / "KNMI" / "20221231.nc")
+    file_path = Path(Path(__file__).parents[2] / "test-data" / "KNMI" / "20221231.nc")
     observation_request_messages = netcdf_file_to_requests(file_path=file_path)
     print(f"Finished creating the time series and observation requests {perf_counter() - create_requests_start}.")
 
