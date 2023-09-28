88af6940893f9cd11560dcfab80dbd53753fb017
Jo Asplin <jo.asplin@met.no>
2023-09-28 12:06:34 +0200

    Reintroduced asserts in example script

--- examples/clients/python/client.py
+++ examples/clients/python/client.py
@@ -20,33 +20,33 @@ def dtime2tstamp(dtime):
 
 
 # callPutObs demonstrates how to insert observations in the datastore.
-def callPutObs(stub):
+def callPutObs(stub, version, type, standard_name, unit, value):
     ts_mdata = dstore.TSMetadata(
-        version='version_dummy',
-        type='type_dummy',
-        standard_name='air_temperature',
-        unit='celsius',
+        version = version,
+        type = type,
+        standard_name = standard_name,
+        unit = unit,
         # add more attributes as required ...
     )
 
     obs_mdata = dstore.ObsMetadata(
-        id='id_dummy',
-        geo_point=dstore.Point(
-            lat=59.91,
-            lon=10.75,
+        id = 'id_dummy',
+        geo_point = dstore.Point(
+            lat = 59.91,
+            lon = 10.75,
         ),
-        pubtime=dtime2tstamp(datetime(2023, 1, 1, 0, 0, 10, 0, tzinfo=timezone.utc)),
-        data_id='data_id_dummy',
-        obstime_instant=dtime2tstamp(datetime(2023, 1, 1, 0, 0, 0, 0, tzinfo=timezone.utc)),
-        value=str(12.7),
+        pubtime = dtime2tstamp(datetime(2023, 1, 1, 0, 0, 10, 0, tzinfo = timezone.utc)),
+        data_id = 'data_id_dummy',
+        obstime_instant = dtime2tstamp(datetime(2023, 1, 1, 0, 0, 0, 0, tzinfo = timezone.utc)),
+        value = value,
         # add more attributes as required ...
     )
 
     request = dstore.PutObsRequest(
-        observations=[  # insert only a single observation for now
+        observations = [  # insert only a single observation for now
             dstore.Metadata1(
-                ts_mdata=ts_mdata,
-                obs_mdata=obs_mdata,
+                ts_mdata = ts_mdata,
+                obs_mdata = obs_mdata,
             )
         ],
     )
@@ -59,9 +59,9 @@ def callPutObs(stub):
 # obs time range.
 def callGetObsInTimeRange(stub):
     request = dstore.GetObsRequest(
-        interval=dstore.TimeInterval(
-            start=dtime2tstamp(datetime(2023, 1, 1, 0, 0, 0, 0, tzinfo=timezone.utc)),
-            end=dtime2tstamp(datetime(2023, 1, 2, 0, 0, 0, 0, tzinfo=timezone.utc)),
+        interval = dstore.TimeInterval(
+            start = dtime2tstamp(datetime(2023, 1, 1, 0, 0, 0, 0, tzinfo = timezone.utc)),
+            end = dtime2tstamp(datetime(2023, 1, 2, 0, 0, 0, 0, tzinfo = timezone.utc)),
         )
     )
     response = stub.GetObservations(request)
@@ -73,12 +73,12 @@ def callGetObsInTimeRange(stub):
 # polygon.
 def callGetObsInPolygon(stub):
     points = []
-    points.append(dstore.Point(lat=59.90, lon=10.70))
-    points.append(dstore.Point(lat=59.90, lon=10.80))
-    points.append(dstore.Point(lat=60, lon=10.80))
-    points.append(dstore.Point(lat=60, lon=10.70))
+    points.append(dstore.Point(lat = 59.90, lon = 10.70))
+    points.append(dstore.Point(lat = 59.90, lon = 10.80))
+    points.append(dstore.Point(lat = 60, lon = 10.80))
+    points.append(dstore.Point(lat = 60, lon = 10.70))
 
-    request = dstore.GetObsRequest(inside=dstore.Polygon(points=points))
+    request = dstore.GetObsRequest(inside  =dstore.Polygon(points = points))
     response = stub.GetObservations(request)
 
     return response
@@ -88,7 +88,13 @@ if __name__ == '__main__':
     with grpc.insecure_channel(f"{os.getenv('DSHOST', 'localhost')}:{os.getenv('DSPORT', '50050')}") as channel:
         stub = dstore_grpc.DatastoreStub(channel)
 
-        response = callPutObs(stub)
+        version = 'version_dummy'
+        type = 'type_dummy'
+        standard_name = 'air_temperature'
+        unit = 'celsius'
+        value = '12.7'
+
+        response = callPutObs(stub, version, type, standard_name, unit, value)
         print('response from callPutObs: {}'.format(response))
 
         response = callGetObsInTimeRange(stub)
@@ -96,3 +102,17 @@ if __name__ == '__main__':
 
         response = callGetObsInPolygon(stub)
         print('response from callGetObsInPolygon: {}'.format(response))
+
+        assert len(response.observations) == 1
+        obs0 = response.observations[0]
+
+        ts_mdata = obs0.ts_mdata
+        assert ts_mdata.version == version
+        assert ts_mdata.type == type
+        assert ts_mdata.standard_name == standard_name
+        assert ts_mdata.unit == unit
+
+        obs_mdata = obs0.obs_mdata
+        assert len(obs_mdata) == 1
+        obs_mdata0 = obs_mdata[0]
+        assert obs_mdata0.value  == value
