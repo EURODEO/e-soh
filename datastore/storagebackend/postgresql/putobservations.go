edea10951b765deba12e86a9f8c077a3c877b263
Jo Asplin <jo.asplin@met.no>
2023-09-28 09:36:11 +0200

    Optimized PutObservations by reducing number of INSERTs
    PutObservations will now (once again!) call INSERT only for
    each time series, not for each observation within a time series.

--- datastore/storagebackend/postgresql/putobservations.go
+++ datastore/storagebackend/postgresql/putobservations.go
@@ -241,23 +241,86 @@ func updateLatestObsTime(
 	return nil
 }
 
-// insertOrUpdateObs inserts a new row or updates an existing row in table observation.
+// createInsertVals generates from (tsID, obsTimes, gpIDs, and omds) two arrays:
+//   - in valsExpr: the list of arguments to the VALUES clause in the SQL INSERT
+//     statement, and
+//   - in phVals: the total, flat list of all placeholder values.
+func createInsertVals(
+	tsID int64, obsTimes *[]*timestamppb.Timestamp, gpIDs *[]int64,
+	omds *[]*datastore.ObsMetadata, valsExpr *[]string, phVals *[]interface{}) {
+	// assert(len(*obsTimes) > 0)
+	// assert(len(*obsTimes) == len(*gpIDs) == len(*omds))
+
+	index := 0
+	for i := 0; i < len(*obsTimes); i++ {
+		valsExpr0 := fmt.Sprintf(`(
+			$%d,
+			to_timestamp($%d),
+			$%d,
+			$%d,
+			to_timestamp($%d),
+			$%d,
+			$%d,
+			$%d,
+			$%d,
+			$%d
+			)`,
+			index + 1,
+			index + 2,
+			index + 3,
+			index + 4,
+			index + 5,
+			index + 6,
+			index + 7,
+			index + 8,
+			index + 9,
+			index + 10,
+		)
+
+		phVals0 := []interface{}{
+			tsID,
+			common.Tstamp2float64Secs((*obsTimes)[i]),
+			(*omds)[i].GetId(),
+			(*gpIDs)[i],
+			common.Tstamp2float64Secs((*omds)[i].GetPubtime()),
+			(*omds)[i].GetDataId(),
+			(*omds)[i].GetHistory(),
+			(*omds)[i].GetMetadataId(),
+			(*omds)[i].GetProcessingLevel(),
+			(*omds)[i].GetValue(),
+		}
+
+		*valsExpr = append(*valsExpr, valsExpr0)
+		*phVals = append(*phVals, phVals0...)
+		index += len(phVals0)
+	}
+}
+
+// upsertObsForTS inserts new observations and/or updates existing ones.
 // Returns nil upon success, otherwise error.
-func insertOrUpdateObs(
-	db *sql.DB, tsID, geoPointID int64, obsTime *timestamppb.Timestamp,
-	obsMdata *datastore.ObsMetadata) error {
+func upsertObsForTS(
+	db *sql.DB, tsID int64, obsTimes *[]*timestamppb.Timestamp, gpIDs *[]int64,
+	omds *[]*datastore.ObsMetadata) error {
 
-	obsTime0 := common.Tstamp2float64Secs(obsTime)
-	id := obsMdata.GetId()
-	pubTime := common.Tstamp2float64Secs(obsMdata.GetPubtime())
-	dataID := obsMdata.GetDataId()
-	history := obsMdata.GetHistory()
-	metadataID := obsMdata.GetMetadataId()
-	processingLevel := obsMdata.GetProcessingLevel()
-	value := obsMdata.GetValue()
+	// assert(obsTimes != nil)
+	if obsTimes == nil {
+		return fmt.Errorf("precondition failed: obsTimes == nil")
+	}
 
-	cmd := fmt.Sprintf(
-		`INSERT INTO observation (
+	// assert(len(*obsTimes) > 0)
+	if len(*obsTimes) == 0 {
+		return fmt.Errorf("precondition failed: len(*obsTimes) == 0")
+	}
+
+	// assert(len(*obsTimes) == len(*gpIDs) == len(*omds))
+	// for now don't check explicitly for this precondition
+
+	valsExpr := []string{}
+	phVals := []interface{}{}
+	createInsertVals(tsID, obsTimes, gpIDs, omds, &valsExpr, &phVals)
+
+	cmd := fmt.Sprintf(`
+		INSERT INTO observation (
 			ts_id,
 			obstime_instant,
 			id,
@@ -268,55 +331,21 @@ func insertOrUpdateObs(
 			metadata_id,
 			processing_level,
 			value)
-		 VALUES ($1, to_timestamp($2), $3, $4, to_timestamp($5), $6, $7, $8, $9, $10)
-		 ON CONFLICT (ts_id, obstime_instant) DO UPDATE SET
-		    id=EXCLUDED.id,
-		 	geo_point_id=EXCLUDED.geo_point_id,
-		 	pubtime=EXCLUDED.pubtime,
-		 	data_id=EXCLUDED.data_id,
-		 	history=EXCLUDED.history,
-		 	metadata_id=EXCLUDED.metadata_id,
-		 	processing_level=EXCLUDED.processing_level,
-		 	value=EXCLUDED.value
-	`) // NOTE that ts_id and obstime_instant form the PK and do not need to be updated
+		VALUES %s
+		ON CONFLICT ON CONSTRAINT observation_pkey DO UPDATE SET
+	    	id = EXCLUDED.id,
+	 		geo_point_id = EXCLUDED.geo_point_id,
+	 		pubtime = EXCLUDED.pubtime,
+	 		data_id = EXCLUDED.data_id,
+	 		history = EXCLUDED.history,
+	 		metadata_id = EXCLUDED.metadata_id,
+	 		processing_level = EXCLUDED.processing_level,
+	 		value = EXCLUDED.value
+	`, strings.Join(valsExpr, ","))
 
-	if _, err := db.Exec(
-		cmd, tsID, obsTime0,
-		id, geoPointID, pubTime, dataID, history, metadataID, processingLevel, value,
-	); err != nil {
+	_, err := db.Exec(cmd, phVals...)
+	if err != nil {
 		return fmt.Errorf("db.Exec() failed: %v", err)
-	}
-
-	return nil
-}
-
-// putObs inserts a new observation or updates an existing one.
-// Returns nil upon success, otherwise error.
-func putObs(db *sql.DB, obs *datastore.Metadata1) error {
-
-	obsTime, err := getObsTime(obs.GetObsMdata())
-	if err != nil {
-		return fmt.Errorf("getObsTime() failed: %v", err)
-	}
-
-	tsID, err := getTimeSeriesID(db, obsTime, obs.GetTsMdata())
-	if err != nil {
-		return fmt.Errorf("getTimeSeriesID() failed: %v", err)
-	}
-
-	geoPointID, err := getGeoPointID(db, obsTime, obs.GetObsMdata().GetGeoPoint())
-	if err != nil {
-		return fmt.Errorf("getGeoPointID() failed: %v", err)
-	}
-
-	err = updateLatestObsTime(db, tsID, geoPointID, obsTime)
-	if err != nil {
-		return fmt.Errorf("updateLatestObsTime() failed: %v", err)
-	}
-
-	err = insertOrUpdateObs(db, tsID, geoPointID, obsTime, obs.GetObsMdata())
-	if err != nil {
-		return fmt.Errorf("insertOrUpdateObs() failed: %v", err)
 	}
 
 	return nil
@@ -325,9 +354,64 @@ func putObs(db *sql.DB, obs *datastore.Metadata1) error {
 // PutObservations ... (see documentation in StorageBackend interface)
 func (sbe *PostgreSQL) PutObservations(request *datastore.PutObsRequest) error {
 
+	type tsInfo struct {
+		obsTimes *[]*timestamppb.Timestamp
+		gpIDs *[]int64 // geo point IDs
+		omds *[]*datastore.ObsMetadata
+	}
+
+	tsInfos := map[int64]tsInfo{}
+
+	// populate tsInfos
 	for _, obs := range request.Observations {
-		if err := putObs(sbe.Db, obs); err != nil {
-			return fmt.Errorf("putObs() failed: %v", err)
+
+		obsTime, err := getObsTime(obs.GetObsMdata())
+		if err != nil {
+			return fmt.Errorf("getObsTime() failed: %v", err)
+		}
+
+		tsID, err := getTimeSeriesID(sbe.Db, obsTime, obs.GetTsMdata())
+		if err != nil {
+			return fmt.Errorf("getTimeSeriesID() failed: %v", err)
+		}
+
+		gpID, err := getGeoPointID(sbe.Db, obsTime, obs.GetObsMdata().GetGeoPoint())
+		if err != nil {
+			return fmt.Errorf("getGeoPointID() failed: %v", err)
+		}
+
+		err = updateLatestObsTime(sbe.Db, tsID, gpID, obsTime)
+		if err != nil {
+			return fmt.Errorf("updateLatestObsTime() failed: %v", err)
+		}
+
+		var obsTimes []*timestamppb.Timestamp
+		var gpIDs []int64
+		var omds []*datastore.ObsMetadata
+		var tsInfo0 tsInfo
+		var found bool
+		if tsInfo0, found = tsInfos[tsID]; !found {
+			obsTimes = []*timestamppb.Timestamp{}
+			gpIDs = []int64{}
+			omds = []*datastore.ObsMetadata{}
+			tsInfos[tsID] = tsInfo{
+				obsTimes: &obsTimes,
+				gpIDs: &gpIDs,
+				omds: &omds,
+			}
+			tsInfo0, found = tsInfos[tsID]
+			// assert(found)
+		}
+		*tsInfo0.obsTimes = append(*tsInfo0.obsTimes, obsTime)
+		*tsInfo0.gpIDs = append(*tsInfo0.gpIDs, gpID)
+		*tsInfo0.omds = append(*tsInfo0.omds, obs.GetObsMdata())
+	}
+
+	// insert observations for each time series
+	for tsID, tsInfo := range tsInfos {
+		if err := upsertObsForTS(
+			sbe.Db, tsID, tsInfo.obsTimes, tsInfo.gpIDs, tsInfo.omds); err != nil {
+			return fmt.Errorf("upsertObsForTS()) failed: %v", err)
 		}
 	}
 
