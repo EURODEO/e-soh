3f50882fd469052f96af63111cf86e7676818701
istvans <istvans@met.no>
2023-10-03 12:14:48 +0200

    BUFR: cpp lint

--- src/esoh/ingest/bufr/Tables.h
+++ src/esoh/ingest/bufr/Tables.h
@@ -1,4 +1,4 @@
- /*
+/*
  * (C) Copyright 2023, met.no
  *
  * This file is part of the Norbufr BUFR en/decoder
@@ -10,85 +10,69 @@
 #ifndef _TABLES_H_
 #define _TABLES_H_
 
-#include <map>
-#include <vector>
-#include <string>
 #include <list>
+#include <map>
+#include <string>
+#include <vector>
 
 #include "Descriptor.h"
 
+class TableBase {};
 
-class TableBase
-{
-
-
-
-};
-
-class TableA
-{
+class TableA {
 public:
-    TableA();
-    std::string operator[](int);
+  TableA();
+  std::string operator[](int);
 
 private:
-    std::vector<std::string> tableA;
+  std::vector<std::string> tableA;
 };
 
-class TableB
-{
+class TableB {
 public:
-    TableB();
-    TableB(std::string f);
+  TableB();
+  TableB(std::string f);
 
-    const DescriptorMeta & at(DescriptorId d, bool ignore_throw = false) const;
+  const DescriptorMeta &at(DescriptorId d, bool ignore_throw = false) const;
 
 private:
-    void clear();
-    bool readWMO( std::string f);
-    bool readECCodes( std::string filename );
-    std::map<DescriptorId,DescriptorMeta> tableB;
-    DescriptorMeta dm0;
-
+  void clear();
+  bool readWMO(std::string f);
+  bool readECCodes(std::string filename);
+  std::map<DescriptorId, DescriptorMeta> tableB;
+  DescriptorMeta dm0;
 };
 
-class TableC
-{
+class TableC {
 public:
-    TableC();
-    TableC(std::string f);
-    std::string codeStr(DescriptorId di,int c);
+  TableC();
+  TableC(std::string f);
+  std::string codeStr(DescriptorId di, int c);
 
 private:
-    void clear();
-    bool readWMO( std::string f);
-    bool readECCodes( std::string filename );
-    std::map<DescriptorId,std::map<int,std::string> > tableC;
-
+  void clear();
+  bool readWMO(std::string f);
+  bool readECCodes(std::string filename);
+  std::map<DescriptorId, std::map<int, std::string>> tableC;
 };
 
-class TableD
-{
+class TableD {
 public:
-    TableD();
-    TableD(std::string f);
+  TableD();
+  TableD(std::string f);
 
-    std::list<DescriptorId> expandDescriptor(const DescriptorId d, bool recursive=false) const;
-    const std::list<DescriptorId> & at(DescriptorId d, bool ignore_throw = false) const; // TODO: exception: out_of_range
-    ssize_t size() const;
+  std::list<DescriptorId> expandDescriptor(const DescriptorId d,
+                                           bool recursive = false) const;
+  const std::list<DescriptorId> &
+  at(DescriptorId d,
+     bool ignore_throw = false) const; // TODO: exception: out_of_range
+  ssize_t size() const;
 
 private:
-    void clear();
-    bool readWMO( std::string f);
-    bool readECCodes( std::string filename );
-    std::map<DescriptorId,std::list<DescriptorId> > tableD;
-
+  void clear();
+  bool readWMO(std::string f);
+  bool readECCodes(std::string filename);
+  std::map<DescriptorId, std::list<DescriptorId>> tableD;
 };
-
-
-
-
-
 
 #endif
-
--- src/esoh/ingest/bufr/bufresohmsg_py.cpp
+++ src/esoh/ingest/bufr/bufresohmsg_py.cpp
@@ -7,145 +7,157 @@
  *
  */
 
-#include <iostream>
-#include <fstream>
-#include <filesystem>
 #include <algorithm>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
 #include <iterator>
+#include <list>
 #include <sstream>
 #include <string>
-#include <filesystem>
-#include <list>
 
 #include "Descriptor.h"
-#include "Tables.h"
 #include "ESOHBufr.h"
+#include "Tables.h"
 #include "bufresohmsg_py.h"
 
-bool norbufr_init_bufrtables(std::string tables_dir)
-{
+bool norbufr_init_bufrtables(std::string tables_dir) {
 
-    std::string tbl_dir;
-    if (tables_dir.size())
-    {
-        tbl_dir = tables_dir;
-    }
-    else
-    {
-        tbl_dir = "/usr/share/eccodes/definitions/bufr/tables/0/wmo";
-    }
+  std::string tbl_dir;
+  if (tables_dir.size()) {
+    tbl_dir = tables_dir;
+  } else {
+    tbl_dir = "/usr/share/eccodes/definitions/bufr/tables/0/wmo";
+  }
 
-    std::string eccBtable_dir(tbl_dir);
-    std::string eccDtable_dir(tbl_dir);
+  std::string eccBtable_dir(tbl_dir);
+  std::string eccDtable_dir(tbl_dir);
 
-    for (const auto &entry : std::filesystem::directory_iterator(eccBtable_dir))
-    {
-        auto vers = std::stoi(entry.path().filename().string());
-        TableB *tb_e = new TableB(entry.path().string() + "/element.table");
-        (*tb)[vers] = tb_e;
-        TableC *tc_e = new TableC(entry.path().string() + "/codetables");
-        (*tc)[vers] = tc_e;
-    }
+  for (const auto &entry : std::filesystem::directory_iterator(eccBtable_dir)) {
+    auto vers = std::stoi(entry.path().filename().string());
+    TableB *tb_e = new TableB(entry.path().string() + "/element.table");
+    (*tb)[vers] = tb_e;
+    TableC *tc_e = new TableC(entry.path().string() + "/codetables");
+    (*tc)[vers] = tc_e;
+  }
 
-    for (const auto &entry : std::filesystem::directory_iterator(eccDtable_dir))
-    {
-        auto vers = std::stoi(entry.path().filename().string());
-        TableD *tb_d = new TableD(entry.path().string() + "/sequence.def");
-        (*td)[vers] = tb_d;
-    }
+  for (const auto &entry : std::filesystem::directory_iterator(eccDtable_dir)) {
+    auto vers = std::stoi(entry.path().filename().string());
+    TableD *tb_d = new TableD(entry.path().string() + "/sequence.def");
+    (*td)[vers] = tb_d;
+  }
 
-    return true;
+  return true;
 }
 
-bool norbufr_init_oscar(std::string oscardb_dir)
-{
-    bool ret = oscar.addStation(oscardb_dir.c_str());
-    return ret;
+bool norbufr_init_oscar(std::string oscardb_dir) {
+  bool ret = oscar.addStation(oscardb_dir.c_str());
+  return ret;
 }
 
-std::list<std::string> norbufr_bufresohmsg(std::string fname)
-{
+std::list<std::string> norbufr_bufresohmsg(std::string fname) {
 
-    std::list<std::string> ret;
+  std::list<std::string> ret;
 
-    std::ifstream bufrFile(fname.c_str(), std::ios_base::in | std::ios_base::binary);
+  std::ifstream bufrFile(fname.c_str(),
+                         std::ios_base::in | std::ios_base::binary);
 
-    while (bufrFile.good())
-    {
+  while (bufrFile.good()) {
 
-        ESOHBufr *bufr = new ESOHBufr;
-        bufr->setOscar(&oscar);
+    ESOHBufr *bufr = new ESOHBufr;
+    bufr->setOscar(&oscar);
 
-        if (bufrFile >> *bufr)
-        {
+    if (bufrFile >> *bufr) {
 
-            bufr->setTableB(tb->at(bufr->getVersionMaster() && tb->find(bufr->getVersionMaster()) != tb->end() ? bufr->getVersionMaster() : tb->rbegin()->first));
-            bufr->setTableC(tc->at(bufr->getVersionMaster() && tc->find(bufr->getVersionMaster()) != tc->end() ? bufr->getVersionMaster() : tc->rbegin()->first));
-            bufr->setTableD(td->at(bufr->getVersionMaster() && td->find(bufr->getVersionMaster()) != td->end() ? bufr->getVersionMaster() : td->rbegin()->first));
+      bufr->setTableB(
+          tb->at(bufr->getVersionMaster() &&
+                         tb->find(bufr->getVersionMaster()) != tb->end()
+                     ? bufr->getVersionMaster()
+                     : tb->rbegin()->first));
+      bufr->setTableC(
+          tc->at(bufr->getVersionMaster() &&
+                         tc->find(bufr->getVersionMaster()) != tc->end()
+                     ? bufr->getVersionMaster()
+                     : tc->rbegin()->first));
+      bufr->setTableD(
+          td->at(bufr->getVersionMaster() &&
+                         td->find(bufr->getVersionMaster()) != td->end()
+                     ? bufr->getVersionMaster()
+                     : td->rbegin()->first));
 
-            bufr->extractDescriptors();
+      bufr->extractDescriptors();
 
-            std::list<std::string> msg = bufr->msg();
-            ret.insert(ret.end(), msg.begin(), msg.end());
-        }
+      std::list<std::string> msg = bufr->msg();
+      ret.insert(ret.end(), msg.begin(), msg.end());
     }
+  }
 
-    return ret;
+  return ret;
 }
 
-std::string norbufr_bufrprint(std::string fname)
-{
+std::string norbufr_bufrprint(std::string fname) {
 
-    std::stringstream ret;
+  std::stringstream ret;
 
-    std::ifstream bufrFile(fname.c_str(), std::ios_base::in | std::ios_base::binary);
+  std::ifstream bufrFile(fname.c_str(),
+                         std::ios_base::in | std::ios_base::binary);
 
-    while (bufrFile.good())
-    {
+  while (bufrFile.good()) {
 
-        ESOHBufr *bufr = new ESOHBufr;
+    ESOHBufr *bufr = new ESOHBufr;
 
-        if (bufrFile >> *bufr)
-        {
+    if (bufrFile >> *bufr) {
 
-            bufr->setTableB(tb->at(bufr->getVersionMaster() && tb->find(bufr->getVersionMaster()) != tb->end() ? bufr->getVersionMaster() : tb->rbegin()->first));
-            bufr->setTableC(tc->at(bufr->getVersionMaster() && tc->find(bufr->getVersionMaster()) != tc->end() ? bufr->getVersionMaster() : tc->rbegin()->first));
-            bufr->setTableD(td->at(bufr->getVersionMaster() && td->find(bufr->getVersionMaster()) != td->end() ? bufr->getVersionMaster() : td->rbegin()->first));
+      bufr->setTableB(
+          tb->at(bufr->getVersionMaster() &&
+                         tb->find(bufr->getVersionMaster()) != tb->end()
+                     ? bufr->getVersionMaster()
+                     : tb->rbegin()->first));
+      bufr->setTableC(
+          tc->at(bufr->getVersionMaster() &&
+                         tc->find(bufr->getVersionMaster()) != tc->end()
+                     ? bufr->getVersionMaster()
+                     : tc->rbegin()->first));
+      bufr->setTableD(
+          td->at(bufr->getVersionMaster() &&
+                         td->find(bufr->getVersionMaster()) != td->end()
+                     ? bufr->getVersionMaster()
+                     : td->rbegin()->first));
 
-            bufr->extractDescriptors();
+      bufr->extractDescriptors();
 
-            ret << *bufr;
-        }
+      ret << *bufr;
     }
+  }
 
-    return ret.str();
+  return ret.str();
 }
 
-bool norbufr_destroy_bufrtables()
-{
+bool norbufr_destroy_bufrtables() {
 
-    for (auto i : *tb)
-        delete i.second;
-    delete tb;
-    for (auto i : *tc)
-        delete i.second;
-    delete tc;
-    for (auto i : *td)
-        delete i.second;
-    delete td;
+  for (auto i : *tb)
+    delete i.second;
+  delete tb;
+  for (auto i : *tc)
+    delete i.second;
+  delete tc;
+  for (auto i : *td)
+    delete i.second;
+  delete td;
 
-    return true;
+  return true;
 }
 
-PYBIND11_MODULE(bufresohmsg_py, m)
-{
-    m.doc() = "bufresoh E-SOH MQTT message generator plugin";
+PYBIND11_MODULE(bufresohmsg_py, m) {
+  m.doc() = "bufresoh E-SOH MQTT message generator plugin";
 
-    m.def("init_bufrtables_py", &norbufr_init_bufrtables, "Init BUFR Tables");
-    m.def("destroy_bufrtables_py", &norbufr_destroy_bufrtables, "Destroy BUFR Tables");
+  m.def("init_bufrtables_py", &norbufr_init_bufrtables, "Init BUFR Tables");
+  m.def("destroy_bufrtables_py", &norbufr_destroy_bufrtables,
+        "Destroy BUFR Tables");
 
-    m.def("bufresohmsg_py", &norbufr_bufresohmsg, "bufresoh MQTT message generator");
-    m.def("bufrprint_py", &norbufr_bufrprint, "Print bufr message");
+  m.def("bufresohmsg_py", &norbufr_bufresohmsg,
+        "bufresoh MQTT message generator");
+  m.def("bufrprint_py", &norbufr_bufrprint, "Print bufr message");
 
-    m.def("init_oscar_py", &norbufr_init_oscar, "Init OSCAR db");
+  m.def("init_oscar_py", &norbufr_init_oscar, "Init OSCAR db");
 }
