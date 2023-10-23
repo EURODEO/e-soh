6078f50f307c72c242c42c43bed670ed123148e7
Jeffrey Vervoort <jeffrey.vervoort@knmi.nl>
2023-10-23 12:04:39 +0200

    Add Go to the local pre-commit hook configuration.

--- ci/go/go-vet.sh
+++ ci/go/go-vet.sh
@@ -6,7 +6,7 @@ output=$(cd ./datastore && go vet ./... 2>&1)
 
 if [ -n "${output}" ]; then
   echo "${output}"
-#  exit 1
+  exit 1
 else
   echo "No subtle issues found in the code. All is working as intended."
 fi
