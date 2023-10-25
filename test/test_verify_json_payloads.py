374b1e8410ecfaacb7eaf7fa5e861287af2f4cc0
istvans <istvans@met.no>
2023-10-25 14:36:01 +0200

    BUFR: style correction

--- test/test_verify_json_payloads.py
+++ test/test_verify_json_payloads.py
@@ -13,6 +13,7 @@ from esoh.ingest.bufr.bufresohmsg_py import init_bufrtables_py, \
     destroy_bufrtables_py, \
     init_bufr_schema_py
 
+
 @pytest.mark.timeout(300)
 @pytest.mark.parametrize("bufr_file_path",
                          glob.glob("test/test_data/bufr/*.buf*")
