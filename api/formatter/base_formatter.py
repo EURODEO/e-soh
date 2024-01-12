f776440eb043916eebc2b02687bc0880ce2cf167
Amund Isaksen <amundi@met.no>
2024-01-12 15:11:37 +0100

    Implemented plugin loader

--- api/formatter/base_formatter.py
+++ api/formatter/base_formatter.py
@@ -4,11 +4,12 @@ from abc import ABC, abstractmethod
 class EDR_formatter(ABC):
     """
     This is the abstract class for implementing a formatter in the E-SOH EDR formatter
+    Name of class should represent expected output format.
     """
     pass
 
     @abstractmethod
-    def convert():
+    def convert(self, datastore_reply):
         """
         Main method for converting protobuf object to given format.
         """
