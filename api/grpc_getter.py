5a164d759b34e941ab2951b982bfdee5cab848e1
Amund Isaksen <amundi@met.no>
2024-01-23 16:32:58 +0100

    Revert to insecure_channel

--- api/grpc_getter.py
+++ api/grpc_getter.py
@@ -9,9 +9,7 @@ import grpc
 
 
 async def getObsRequest(get_obs_request):
-    channel = grpc.aio.secure_channel(
-        f"{os.getenv('DSHOST', 'localhost')}:{os.getenv('DSPORT', '50050')}", grpc.ssl_channel_credentials()
-    )
+    channel = grpc.aio.insecure_channel(f"{os.getenv('DSHOST', 'localhost')}:{os.getenv('DSPORT', '50050')}")
     grpc_stub = dstore_grpc.DatastoreStub(channel)
     response = await grpc_stub.GetObservations(get_obs_request)
 
