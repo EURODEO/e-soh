23ed4193551bbb7b04f07a1887068db0c82fc9d6
Amund Isaksen <amundi@met.no>
2024-01-30 16:17:09 +0100

    Add function for making the getTSAttrGroups async call

--- api/grpc_getter.py
+++ api/grpc_getter.py
@@ -14,3 +14,10 @@ async def getObsRequest(get_obs_request):
     response = await grpc_stub.GetObservations(get_obs_request)
 
     return response
+
+async def getTSAGRequest(get_tsag_request):
+    channel = grpc.aio.insecure_channel(f"{os.getenv('DSHOST', 'localhost')}:{os.getenv('DSPORT', '50050')}")
+    grpc_stub = dstore_grpc.DatastoreStub(channel)
+    response = await grpc_stub.GetTSAttrGroups(get_tsag_request)
+
+    return response
