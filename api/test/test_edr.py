a0653f2cf2f3c3ca93888393c512d48a03a6b9a1
jukkap <jukka.pelli@fmi.fi>
2024-02-08 13:34:58 +0200

    Test: add tests for area & position ednpoints

--- api/test/test_edr.py
+++ api/test/test_edr.py
@@ -2,6 +2,7 @@ import json
 from unittest.mock import patch
 
 import datastore_pb2 as dstore
+import routers.edr as edr
 from fastapi.testclient import TestClient
 from google.protobuf.json_format import Parse
 from main import app
@@ -26,6 +27,12 @@ def test_get_locations_id_with_single_parameter_query_without_format():
         mock_getObsRequest.assert_called_once()
         assert "ff" in mock_getObsRequest.call_args[0][0].filter["instrument"].values
         assert "06260" in mock_getObsRequest.call_args[0][0].filter["platform"].values
+        assert "2022-12-31 00:00:00" == mock_getObsRequest.call_args[0][0].interval.start.ToDatetime().strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+        assert "2022-12-31 01:00:01" == mock_getObsRequest.call_args[0][0].interval.end.ToDatetime().strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
 
         assert response.status_code == 200
         assert response.json()["type"] == "Coverage"
@@ -64,6 +71,108 @@ def test_get_locations_id_with_incorrect_datetime_range():
 
     assert response.status_code == 422
     assert response.json() == {"detail": {"datetime": "Invalid range: 2024-12-31T00:00:00Z > 2022-12-31T01:00:00Z"}}
+
+
+def test_get_locations_id_with_empty_response():
+    with patch("routers.edr.getObsRequest") as mock_getObsRequest:
+        test_data = load_json("test/test_data/test_empty_proto.json")
+
+        mock_getObsRequest.return_value = create_mock_obs_response(test_data)
+
+        response = client.get("/collections/observations/locations/10000?f=covjson")
+
+        assert response.status_code == 404
+        assert response.json() == {"detail": "No data found"}
+
+
+def test_get_area_with_normal_query():
+    with patch("routers.edr.getObsRequest") as mock_getObsRequest:
+        test_data = load_json("test/test_data/test_coverages_proto.json")
+        compare_data = load_json("test/test_data/test_coverages_covjson.json")
+
+        mock_getObsRequest.return_value = create_mock_obs_response(test_data)
+
+        response = client.get(
+            "/collections/observations/area?coords=POLYGON((22.12 59.86, 24.39 60.41, "
+            " 24.39 60.41, 24.39 59.86, 22.12 59.86))"
+            "&parameter-name=TA_P1D_AVG&datetime=2022-12-31T00:00:00Z/2022-12-31T01:00:00Z"
+        )
+
+        # Check that getObsRequest gets called with correct arguments given in query
+        mock_getObsRequest.assert_called_once()
+        assert "TA_P1D_AVG" in mock_getObsRequest.call_args[0][0].filter["instrument"].values
+        assert len(mock_getObsRequest.call_args[0][0].inside.points) == 5
+        assert 22.12 == mock_getObsRequest.call_args[0][0].inside.points[0].lon
+        assert "2022-12-31 00:00:00" == mock_getObsRequest.call_args[0][0].interval.start.ToDatetime().strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+        assert "2022-12-31 01:00:01" == mock_getObsRequest.call_args[0][0].interval.end.ToDatetime().strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
+
+        assert response.status_code == 200
+        assert response.json() == compare_data
+
+
+def test_get_area_with_incorrect_coords():
+    response = client.get("/collections/observations/area?coords=POLYGON((22.12 59.86, 24.39 60.41))")
+
+    assert response.status_code == 422
+
+
+def test_get_area_with_incorrect_geometry_type():
+    response = client.get("/collections/observations/area?coords=POINT(22.12 59.86)")
+
+    assert response.status_code == 422
+
+
+def test_get_position_with_normal_query():
+    # Wrap the original get_data_area to a mock so we can assert against the call values
+    with patch("routers.edr.get_data_area", wraps=edr.get_data_area) as mock_get_data_area, patch(
+        "routers.edr.getObsRequest"
+    ) as mock_getObsRequest:
+        test_data = load_json("test/test_data/test_coverages_proto.json")
+        compare_data = load_json("test/test_data/test_coverages_covjson.json")
+
+        mock_getObsRequest.return_value = create_mock_obs_response(test_data)
+
+        response = client.get(
+            "/collections/observations/position?coords=POINT(5.179705 52.0988218)"
+            "&parameter-name=TA_P1D_AVG&datetime=2022-12-31T00:00Z/2022-12-31T00:00Z"
+        )
+
+        mock_get_data_area.assert_called_once()
+        mock_get_data_area.assert_called_with(
+            "POLYGON ((5.179805 52.0988218, 5.179705 52.0987218, 5.1796050000000005 52.0988218, "
+            "5.179705 52.09892180000001, 5.179805 52.0988218))",
+            "TA_P1D_AVG",
+            "2022-12-31T00:00Z/2022-12-31T00:00Z",
+            "covjson",
+        )
+
+        assert response.status_code == 200
+        assert response.json() == compare_data
+
+
+def test_get_position_with_incorrect_coords():
+    response = client.get("/collections/observations/position?coords=POINT(60.41)")
+
+    assert response.status_code == 422
+    assert response.json() == {"detail": {"coords": "Invalid coordinates: POINT(60.41)"}}
+
+
+def test_get_position_with_incorrect_geometry_type():
+    response = client.get(
+        "/collections/observations/position?coords=POLYGON((22.12 59.86, 24.39 60.41, "
+        "24.39 60.41, 24.39 59.86, 22.12 59.86))"
+    )
+
+    assert response.status_code == 422
+    assert response.json() == {
+        "detail": {
+            "coords": "Invalid coordinates: POLYGON((22.12 59.86, 24.39 60.41, 24.39 60.41, 24.39 59.86, 22.12 59.86))"
+        }
+    }
 
 
 def create_mock_obs_response(json_data):
