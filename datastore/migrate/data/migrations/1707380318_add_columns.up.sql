f838e0086ef8a449cdfcb4d48be03a86609531a8
Jo Asplin <jo.asplin@met.no>
2024-02-09 15:44:06 +0100

    Added parameter_name and related fields
    The parameter_name field is intended to be a human-readable,
    representation of the "minimum" set of metadata fields that in
    practice will often suffice for deciding what is measured by a
    given time_series.
    
    It will have the following form:
    
      <standard name>_<level>_<function>_<period>
    
    Note that currently only standard_name is an official ACDD attribute.
    
    Note also that two different time series may have the same
    parameter_name (and differ e.g. in the instrument field).

---
+++ datastore/migrate/data/migrations/1707380318_add_columns.up.sql
@@ -0,0 +1,11 @@
+ALTER TABLE time_series
+	ADD COLUMN level TEXT,
+	ADD COLUMN period TEXT,
+	ADD COLUMN function TEXT,
+	ADD COLUMN parameter_name TEXT,
+	DROP CONSTRAINT unique_main,
+	ADD CONSTRAINT unique_main UNIQUE NULLS NOT DISTINCT (version, type, title, summary, keywords,
+		keywords_vocabulary, license, conventions, naming_authority, creator_type, creator_name,
+		creator_email, creator_url, institution, project, source, platform, platform_vocabulary,
+		standard_name, unit, instrument, instrument_vocabulary, level, period, function,
+		parameter_name, link_href, link_rel, link_type, link_hreflang, link_title);
