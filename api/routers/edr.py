8d074b2284db76227febf42bbc9f81e09409067a
Amund Isaksen <amundi@met.no>
2024-02-21 15:13:43 +0100

    Update example parameter_name in edr

--- api/routers/edr.py
+++ api/routers/edr.py
@@ -52,7 +52,10 @@ async def get_locations(
             properties=None,
             geometry=Point(
                 type="Point",
-                coordinates=(ts.obs_mdata[0].geo_point.lon, ts.obs_mdata[0].geo_point.lat),
+                coordinates=(
+                    ts.obs_mdata[0].geo_point.lon,
+                    ts.obs_mdata[0].geo_point.lat,
+                ),
             ),
         )  # HACK: Assume loc the same
         for ts in sorted(ts_response.observations, key=lambda ts: ts.ts_mdata.platform)
@@ -68,7 +71,17 @@ async def get_locations(
 )
 async def get_data_location_id(
     location_id: Annotated[str, Path(example="06260")],
-    parameter_name: Annotated[str | None, Query(alias="parameter-name", example="dd,ff,rh,pp,tn")] = None,
+    parameter_name: Annotated[
+        str | None,
+        Query(
+            alias="parameter-name",
+            example="wind_from_direction_2.0_mean_PT10M,"
+            "wind_speed_2.0_mean_PT10M,"
+            "relative_humidity_2.0_mean_PT1M,"
+            "air_pressure_at_sea_level_2.0_mean_PT1M,"
+            "air_temperature_2.0_minimum_PT10M",
+        ),
+    ] = None,
     datetime: Annotated[str | None, Query(example="2022-12-31T00:00Z/2023-01-01T00:00Z")] = None,
     f: Annotated[formatters.Formats, Query(description="Specify return format.")] = formatters.Formats.covjson,
 ):
@@ -81,9 +94,10 @@ async def get_data_location_id(
     # parameter_name = verify_parameter_names(parameter_name) # should the api verify that the parameter name is valid?
     get_obs_request = dstore.GetObsRequest(
         filter=dict(
-            parameter_name=dstore.Strings(values=parameter_name), platform=dstore.Strings(values=[location_id])
+            parameter_name=dstore.Strings(values=parameter_name),
+            platform=dstore.Strings(values=[location_id]),
         ),
-        temporal_interval=dstore.TimeInterval(start=range[0], end=range[1]) if range else None,
+        temporal_interval=(dstore.TimeInterval(start=range[0], end=range[1]) if range else None),
     )
     response = await getObsRequest(get_obs_request)
     return formatters.formatters[f](response)
@@ -97,7 +111,17 @@ async def get_data_location_id(
 )
 async def get_data_position(
     coords: Annotated[str, Query(example="POINT(5.179705 52.0988218)")],
-    parameter_name: Annotated[str | None, Query(alias="parameter-name", example="dd,ff,rh,pp,tn")] = None,
+    parameter_name: Annotated[
+        str | None,
+        Query(
+            alias="parameter-name",
+            example="wind_from_direction_2.0_mean_PT10M,"
+            "wind_speed_2.0_mean_PT10M,"
+            "relative_humidity_2.0_mean_PT1M,"
+            "air_pressure_at_sea_level_2.0_mean_PT1M,"
+            "air_temperature_2.0_minimum_PT10M",
+        ),
+    ] = None,
     datetime: Annotated[str | None, Query(example="2022-12-31T00:00Z/2023-01-01T00:00Z")] = None,
     f: Annotated[formatters.Formats, Query(description="Specify return format.")] = formatters.Formats.covjson,
 ):
@@ -107,9 +131,15 @@ async def get_data_position(
             raise TypeError
         poly = buffer(point, 0.0001, quad_segs=1)  # Roughly 10 meters around the point
     except GEOSException:
-        raise HTTPException(status_code=400, detail={"coords": f"Invalid or unparseable wkt provided: {coords}"})
+        raise HTTPException(
+            status_code=400,
+            detail={"coords": f"Invalid or unparseable wkt provided: {coords}"},
+        )
     except TypeError:
-        raise HTTPException(status_code=400, detail={"coords": f"Invalid geometric type: {point.geom_type}"})
+        raise HTTPException(
+            status_code=400,
+            detail={"coords": f"Invalid geometric type: {point.geom_type}"},
+        )
     except Exception:
         raise HTTPException(
             status_code=400,
@@ -127,7 +157,17 @@ async def get_data_position(
 )
 async def get_data_area(
     coords: Annotated[str, Query(example="POLYGON((5.0 52.0, 6.0 52.0,6.0 52.1,5.0 52.1, 5.0 52.0))")],
-    parameter_name: Annotated[str | None, Query(alias="parameter-name", example="dd,ff,rh,pp,tn")] = None,
+    parameter_name: Annotated[
+        str | None,
+        Query(
+            alias="parameter-name",
+            example="wind_from_direction_2.0_mean_PT10M,"
+            "wind_speed_2.0_mean_PT10M,"
+            "relative_humidity_2.0_mean_PT1M,"
+            "air_pressure_at_sea_level_2.0_mean_PT1M,"
+            "air_temperature_2.0_minimum_PT10M",
+        ),
+    ] = None,
     datetime: Annotated[str | None, Query(example="2022-12-31T00:00Z/2023-01-01T00:00Z")] = None,
     f: Annotated[formatters.Formats, Query(description="Specify return format.")] = formatters.Formats.covjson,
 ):
@@ -136,9 +176,15 @@ async def get_data_area(
         if poly.geom_type != "Polygon":
             raise TypeError
     except GEOSException:
-        raise HTTPException(status_code=400, detail={"coords": f"Invalid or unparseable wkt provided: {coords}"})
+        raise HTTPException(
+            status_code=400,
+            detail={"coords": f"Invalid or unparseable wkt provided: {coords}"},
+        )
     except TypeError:
-        raise HTTPException(status_code=400, detail={"coords": f"Invalid geometric type: {poly.geom_type}"})
+        raise HTTPException(
+            status_code=400,
+            detail={"coords": f"Invalid geometric type: {poly.geom_type}"},
+        )
     except Exception:
         raise HTTPException(
             status_code=400,
@@ -152,7 +198,7 @@ async def get_data_area(
         spatial_area=dstore.Polygon(
             points=[dstore.Point(lat=coord[1], lon=coord[0]) for coord in poly.exterior.coords]
         ),
-        temporal_interval=dstore.TimeInterval(start=range[0], end=range[1]) if range else None,
+        temporal_interval=(dstore.TimeInterval(start=range[0], end=range[1]) if range else None),
     )
     coverages = await getObsRequest(get_obs_request)
     coverages = formatters.formatters[f](coverages)
