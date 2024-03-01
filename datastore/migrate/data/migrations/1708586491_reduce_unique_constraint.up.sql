1b817fd3463615f2b5772b593fc1f7604e4a6d53
Jo Asplin <jo.asplin@met.no>
2024-03-01 10:21:47 +0100

    Merged migration files into a single up/down pair for now

--- datastore/migrate/data/migrations/1708586491_reduce_unique_constraint.up.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE time_series
-	DROP CONSTRAINT IF EXISTS unique_main,
-	ADD CONSTRAINT unique_main UNIQUE NULLS NOT DISTINCT (naming_authority, platform,
-		standard_name, instrument, level, period, function);
