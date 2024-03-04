4056f7dd22e512582a954b23e5abd0218fe11bee
Jeffrey Vervoort <jeffrey.vervoort@knmi.nl>
2024-03-04 15:21:23 +0100

    Add ToDo EDR Feature Collection.

--- api/custom_geo_json/edr_feature_collection.py
+++ api/custom_geo_json/edr_feature_collection.py
@@ -5,5 +5,7 @@ from edr_pydantic.parameter import EdrBaseModel
 from geojson_pydantic import FeatureCollection
 
 
+# TODO: In the future move this to https://github.com/KNMI/edr-pydantic
+#  some work was already done in https://github.com/KNMI/edr-pydantic/pull/7
 class EDRFeatureCollection(EdrBaseModel, FeatureCollection):
     parameters: Dict[str, Parameter]
