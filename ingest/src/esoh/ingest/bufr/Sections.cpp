4cb7b1f02672c6c376cb5358694192abf368a45d
istvans <istvans@met.no>
2024-03-20 08:19:59 +0100

    Style corr

--- ingest/src/esoh/ingest/bufr/Sections.cpp
+++ ingest/src/esoh/ingest/bufr/Sections.cpp
@@ -265,7 +265,8 @@ std::ostream &operator<<(std::ostream &os, Section3 &sec) {
      << "\n";
   os << "Subsets: " << sec.subsets << "\n";
   os << "Observed: " << sec.isObserved() << "\n";
-  os << "Compressed:" << sec.isCompressed() << " " << "\n";
+  os << "Compressed:" << sec.isCompressed() << " " 
+     << "\n";
 
   os << "Descriptors:\n";
 
