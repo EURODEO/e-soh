58deef098671da1fbfadc93966563d2256387834
shamly majeed <shamlym@met.no>
2024-03-21 11:42:50 +0100

    update the mqtt message generation for bufr files

--- ingest/src/ingest/bufr/create_mqtt_message_from_bufr.py
+++ ingest/src/ingest/bufr/create_mqtt_message_from_bufr.py
@@ -3,15 +3,18 @@ import json
 import os
 import sys
 
-from ingest.bufr.bufresohmsg_py import bufresohmsg_py
+from ingest.bufr.bufresohmsg_py import bufresohmsgmem_py
 from ingest.bufr.bufresohmsg_py import bufrlog_clear_py
 from ingest.bufr.bufresohmsg_py import bufrlog_py
 from ingest.bufr.bufresohmsg_py import init_bufr_schema_py
 from ingest.bufr.bufresohmsg_py import init_bufrtables_py
 from ingest.bufr.bufresohmsg_py import init_oscar_py
 
+init_bufrtables_py("/usr/share/eccodes/definitions/bufr/tables/0/wmo/")
+init_oscar_py("./src/ingest/bufr/oscar/oscar_stations_all.json")
 
-def build_all_json_payloads_from_bufr(bufr_file_path: str) -> list[str]:
+
+def build_all_json_payloads_from_bufr(bufr_content: object) -> list[str]:
     """
     This function creates the e-soh-message-spec json schema(s) from a BUFR file.
 
@@ -25,7 +28,9 @@ def build_all_json_payloads_from_bufr(bufr_file_path: str) -> list[str]:
     ---
     """
     ret_str = []
-    msg_str_list = bufresohmsg_py(bufr_file_path)
+
+    msg_str_list = bufresohmsgmem_py(bufr_content, len(bufr_content))
+
     for json_str in msg_str_list:
         json_bufr_msg = json.loads(json_str)
         ret_str.append(copy.deepcopy(json_bufr_msg))
@@ -34,19 +39,21 @@ def build_all_json_payloads_from_bufr(bufr_file_path: str) -> list[str]:
 
 
 def bufr2mqtt(bufr_file_path: str = "") -> list[str]:
-    ret_str = bufresohmsg_py(bufr_file_path)
+    # ret_str = bufresohmsg_py(bufr_file_path)
+    with open(bufr_file_path, "rb") as file:
+        bufr_content = file.read()
+    # print(type(bufr_content))
+    print(len(bufr_content))
+    ret_str = bufresohmsgmem_py(bufr_content, len(bufr_content))
     return ret_str
 
 
 if __name__ == "__main__":
     test_path = "./test/test_data/bufr/SYNOP_BUFR_2718.bufr"
-    test_schema_path = "./src/esoh/schemas/bufr_to_e_soh_message.json"
+    test_schema_path = "./src/ingest/schemas/bufr_to_e_soh_message.json"
     msg = ""
 
-    # init_bufrtables_py("/usr/share/eccodes/definitions/bufr/tables/0/wmo/")
-    init_bufrtables_py("")
-    init_oscar_py("./src/esoh/ingest/bufr/oscar/oscar_stations_all.json")
-    init_bufr_schema_py("./src/esoh/schemas/bufr_to_e_soh_message.json")
+    init_bufr_schema_py("./src/ingest/schemas/bufr_to_e_soh_message.json")
 
     if len(sys.argv) > 1:
         for i, file_name in enumerate(sys.argv):
