79b34bbf7cbf14a9d316202bc7626fab388af118
Jeffrey Vervoort <jeffrey.vervoort@knmi.nl>
2024-03-21 14:20:45 +0100

    Change example to examples because of deprecation.

--- api/routers/feature.py
+++ api/routers/feature.py
@@ -32,11 +32,11 @@ env = Environment(loader=FileSystemLoader("templates"), autoescape=select_autoes
     response_class=GeoJsonResponse,
 )
 async def search_timeseries(
-    bbox: Annotated[str | None, Query(example="5.0,52.0,6.0,52.1")] = None,
+    bbox: Annotated[str | None, Query(examples="5.0,52.0,6.0,52.1")] = None,
     datetime: Annotated[
         str | None,
         Query(
-            example="2022-12-31T00:00Z/2023-01-01T00:00Z",
+            examples="2022-12-31T00:00Z/2023-01-01T00:00Z",
             description="E-SOH database only contains data from the last 24 hours",
         ),
     ] = None,
@@ -44,29 +44,29 @@ async def search_timeseries(
     parameter_name: Annotated[str | None, Query(alias="parameter-name", description="E-SOH parameter name")] = None,
     naming_authority: Annotated[
         str | None,
-        Query(alias="naming-authority", description="Naming authority that created the data", example="no.met"),
+        Query(alias="naming-authority", description="Naming authority that created the data", examples="no.met"),
     ] = None,
     institution: Annotated[
         str | None,
-        Query(description="Institution that published the data", example="Norwegian meterological institution"),
+        Query(description="Institution that published the data", examples="Norwegian meterological institution"),
     ] = None,
     platform: Annotated[
-        str | None, Query(description="Platform ID, WIGOS or WIGOS equivalent.", example="0-20000-0-01492")
+        str | None, Query(description="Platform ID, WIGOS or WIGOS equivalent.", examples="0-20000-0-01492")
     ] = None,
     standard_name: Annotated[
-        str | None, Query(alias="standard-name", description="CF 1.9 standard name", example="air_temperature")
+        str | None, Query(alias="standard-name", description="CF 1.9 standard name", examples="air_temperature")
     ] = None,
-    unit: Annotated[str | None, Query(description="Unit of observed physical property", example="degC")] = None,
+    unit: Annotated[str | None, Query(description="Unit of observed physical property", examples="degC")] = None,
     instrument: Annotated[str | None, Query(description="Instrument Id")] = None,
     level: Annotated[
         str | None,
-        Query(description="Instruments height above ground or distance below surface, in meters", example=2),
+        Query(description="Instruments height above ground or distance below surface, in meters", examples=2),
     ] = None,
     period: Annotated[
-        str | None, Query(description="Duration of collection period in ISO8601", example="PT10M")
+        str | None, Query(description="Duration of collection period in ISO8601", examples="PT10M")
     ] = None,
     function: Annotated[
-        str | None, Query(description="Aggregation function used to sample observed property", example="maximum")
+        str | None, Query(description="Aggregation function used to sample observed property", examples="maximum")
     ] = None,
     f: Annotated[
         formatters.Metadata_Formats, Query(description="Specify return format")
