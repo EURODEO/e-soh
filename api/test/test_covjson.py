1487566134d28b6ae72ac77ec17f55e0bc501ef3
Lukas Phaf <lukas.phaf@knmi.nl>
2024-03-22 10:48:26 +0100

    Fix the other tests.

--- api/test/test_covjson.py
+++ api/test/test_covjson.py
@@ -9,9 +9,9 @@ from test.utilities import create_mock_obs_response
 from test.utilities import load_json
 
 
+# /collections/observations/position?coords=POINT(6.5848470019087%2053.123676213651)&
+# datetime=2022-12-31T00%3A00%3A00Z%2F2022-12-31T01%3A00%3A00Z&parameter-name=wind_speed%3A10%3Amean%3APT10M
 def test_single_parameter_convert():
-    # /collections/observations/position?coords=POINT(6.5848470019087%2053.123676213651)&
-    # datetime=2022-12-31T00%3A00%3A00Z%2F2022-12-31T01%3A00%3A00Z&parameter-name=wind_speed%3A10%3Amean%3APT10M
     test_data = load_json("test/test_data/test_single_proto.json")
     compare_data = load_json("test/test_data/test_single_covjson.json")
 
@@ -36,11 +36,13 @@ def test_single_parameter_convert():
     )
 
     # compare the coverage collection with the compare data
-    # TODO: Modify compare data when parameter names have been decided
     coverage_collection_json = json.loads(coverage_collection.model_dump_json(exclude_none=True))
     assert coverage_collection_json == compare_data
 
 
+# /collections/observations/position?coords=POINT(6.5848470019087%2053.123676213651)&
+# parameter-name=wind_from_direction%3A2.0%3Amean%3APT10M,wind_speed%3A10%3Amean%3APT10M,relative_humidity%3A2.0%3Amean%3APT1M&
+# datetime=2022-12-31T00%3A00%3A00Z%2F2022-12-31T00%3A20%3A00Z
 def test_multiple_parameter_convert():
     test_data = load_json("test/test_data/test_multiple_proto.json")
     compare_data = load_json("test/test_data/test_multiple_covjson.json")
@@ -54,19 +56,22 @@ def test_multiple_parameter_convert():
     assert type(coverage_collection) is Coverage
 
     # Check that the coverage collection has the correct parameters
-    # TODO: Change parameter names when parameter names have been decided
-    assert set(["dd", "ff", "rh"]) == coverage_collection.parameters.keys()
+    assert (
+        set(["wind_from_direction:2.0:mean:PT10M", "wind_speed:10:mean:PT10M", "relative_humidity:2.0:mean:PT1M"])
+        == coverage_collection.parameters.keys()
+    )
 
     # Check that correct values exist in the coverage collection
-    assert 230.7 in coverage_collection.ranges["dd"].values
-    assert 9.19 in coverage_collection.ranges["ff"].values
-    assert 88.0 in coverage_collection.ranges["rh"].values
+    assert 230.7 in coverage_collection.ranges["wind_from_direction:2.0:mean:PT10M"].values
+    assert 9.19 in coverage_collection.ranges["wind_speed:10:mean:PT10M"].values
+    assert 88.0 in coverage_collection.ranges["relative_humidity:2.0:mean:PT1M"].values
 
-    # TODO: Modify compare data when parameter names have been decided
     coverage_collection_json = json.loads(coverage_collection.model_dump_json(exclude_none=True))
     assert coverage_collection_json == compare_data
 
 
+# /collections/observations/area?coords=POLYGON((4.0%2052.4,%205.8%2052.4,5.8%2052.6,4.0%2052.6,%204.0%2052.4))&
+# datetime=2022-12-31T00%3A00%3A00Z&parameter-name=air_temperature%3A2.0%3Amean%3APT1M
 def test_single_parameter_area_convert():
     test_data = load_json("test/test_data/test_coverages_proto.json")
     compare_data = load_json("test/test_data/test_coverages_covjson.json")
@@ -84,10 +89,10 @@ def test_single_parameter_area_convert():
     assert all([type(coverage) is Coverage for coverage in coverage_collection.coverages])
 
     # Check that each coverage has the correct parameter
-    # TODO: Change parameter name when parameter names have been decided
-    assert all(["TA_P1D_AVG" in coverage.parameters.keys() for coverage in coverage_collection.coverages])
+    assert all(
+        ["air_temperature:2.0:mean:PT1M" in coverage.parameters.keys() for coverage in coverage_collection.coverages]
+    )
 
-    # TODO: Modify compare data when parameter names have been decided
     coverage_collection_json = json.loads(coverage_collection.model_dump_json(exclude_none=True))
     assert coverage_collection_json == compare_data
 
