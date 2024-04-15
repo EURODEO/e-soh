16048ce32da372824e206a8aa6514162f2408bbf
shamly majeed <shamlym@met.no>
2024-04-15 13:05:29 +0200

    Fixing test failures after changing validation using pydantic.

--- ingest/test/test_verify_json_payloads.py
+++ ingest/test/test_verify_json_payloads.py
@@ -3,10 +3,9 @@ import json
 
 import pytest
 import xarray as xr
-from jsonschema import Draft202012Validator
 from jsonschema import ValidationError
-
-from api.ingest import IngestToPipeline
+from api.model import JsonMessageSchema
+from api.messages import build_all_json_payloads_from_bufr, build_all_json_payloads_from_netcdf
 from ingest.bufr.bufresohmsg_py import init_bufr_schema_py
 from ingest.bufr.bufresohmsg_py import init_bufrtables_py
 from ingest.bufr.bufresohmsg_py import init_oscar_py
@@ -21,44 +20,42 @@ mqtt_configuration = {
 
 @pytest.mark.timeout(1000)
 @pytest.mark.parametrize("bufr_file_path", glob.glob("test/test_data/bufr/*.buf*"))
-def test_verify_json_payload_bufr(bufr_file_path):
-    # Load the schema
-    with open("src/ingest/schemas/e-soh-message-spec.json", "r") as file:
-        e_soh_mqtt_message_schema = json.load(file)
+def test_verify_json_payload_bufr(bufr_file_path, capsys):
 
     init_bufrtables_py("")
     init_oscar_py("./src/ingest/bufr/oscar/oscar_stations_all.json")
     init_bufr_schema_py("./src/ingest/schemas/bufr_to_e_soh_message.json")
 
-    msg_build = IngestToPipeline(mqtt_configuration, "testing")
     with open(bufr_file_path, "rb") as file:
         bufr_content = file.read()
 
-    json_payloads = msg_build._build_messages(bufr_content, input_type="bufr")
+    json_payloads = build_all_json_payloads_from_bufr(bufr_content)
 
     for payload in json_payloads:
         try:
-            assert Draft202012Validator(e_soh_mqtt_message_schema).validate(payload) is None
+            instant = JsonMessageSchema.model_validate_json(json.dumps(payload))
+
+            assert instant is not None
+            assert isinstance(instant, JsonMessageSchema)
         except ValidationError as e:
             print(e)
             raise ValidationError(e)
 
 
 @pytest.mark.parametrize("netcdf_file_path", glob.glob("test/test_data/met_norway/*.nc"))
-def test_verify_json_payload_metno_netcdf(netcdf_file_path):
-    # Load the schema
-    with open("src/ingest/schemas/e-soh-message-spec.json", "r") as file:
-        e_soh_mqtt_message_schema = json.load(file)
+@pytest.mark.parametrize("schema_path", glob.glob("./src/ingest/schemas"))
+def test_verify_json_payload_metno_netcdf(netcdf_file_path, schema_path):
 
     ds = xr.load_dataset(netcdf_file_path)
 
-    msg_build = IngestToPipeline(mqtt_configuration, "testing")
-
-    json_payloads = msg_build._build_messages(ds, input_type="netCDF")
+    json_payloads = build_all_json_payloads_from_netcdf(ds, schema_path)
 
     for payload in json_payloads:
         try:
-            assert Draft202012Validator(e_soh_mqtt_message_schema).validate(payload) is None
+            instant = JsonMessageSchema.model_validate_json(json.dumps(payload))
+
+            assert instant is not None
+            assert isinstance(instant, JsonMessageSchema)
         except ValidationError as e:
             print(e)
             raise ValidationError(e)
