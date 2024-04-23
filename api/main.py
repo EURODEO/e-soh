622ab875dd61de36cec07e6da656730c93ec553d
Jeffrey Vervoort <jeffrey.vervoort@knmi.nl>
2024-04-23 17:42:02 +0200

    Hide health endpoint from schema.

--- api/main.py
+++ api/main.py
@@ -41,7 +41,7 @@ async def landing_page(request: Request) -> LandingPageModel:
     return metadata_endpoints.get_landing_page(request)
 
 
-@app.get("/health")
+@app.get("/health", include_in_schema=False)
 def health():
     return "ok"
 
