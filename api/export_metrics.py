ed88f38a24ee1cad034e13bc3b338c457d05ff80
Amund Isaksen <amundi@met.no>
2024-09-16 08:21:45 +0200

    Add metrics endpoint to edr api

---
+++ api/export_metrics.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from prometheus_client import CollectorRegistry
+from prometheus_client import multiprocess
+from prometheus_fastapi_instrumentator import Instrumentator
+
+
+def add_metrics(app: FastAPI):
+    registry = CollectorRegistry()
+    multiprocess.MultiProcessCollector(registry)
+
+    instrumentator = Instrumentator(excluded_handlers=[".*admin.*", "/metrics", "/health"])
+    instrumentator.add()
+    instrumentator.instrument(app).expose(app, include_in_schema=False)
