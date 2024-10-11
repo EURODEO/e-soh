effefc1ef44a113a9ce37f1253d74c0ec50d83aa
Amund Isaksen <amundi@met.no>
2024-10-11 15:09:13 +0200

    Move some utliity and update parameter-name generation

---
+++ ingest/api/utilities.py
@@ -0,0 +1,22 @@
+import isodate
+
+
+def seconds_to_iso_8601_duration(seconds: int) -> str:
+    duration = isodate.Duration(seconds=seconds)
+    iso_duration = isodate.duration_isoformat(duration)
+
+    # TODO: find a better way to format these
+    # Use PT24H instead of P1D
+    if iso_duration == "P1D":
+        iso_duration = "PT24H"
+
+    # iso_duration defaults to P0D when seconds is 0
+    if iso_duration == "P0D":
+        iso_duration = "PT0S"
+
+    return iso_duration
+
+
+def convert_to_meter(level: int) -> str:
+    level = str(float(level) / 100)
+    return level
