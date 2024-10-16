a0d0befdada9e480f621e91db8215bf77fac66ea
Amund Isaksen <amundi@met.no>
2024-10-16 08:54:49 +0200

    Update contact filed

--- api/openapi/openapi_metadata.py
+++ api/openapi/openapi_metadata.py
@@ -3,27 +3,10 @@ openapi_metadata = {
     "description": (
         "EDR data service for European environmental observations from EUMETNET," " co-funded by the European Union"
     ),
-    "contacts": [
-        {
-            "organization": "EUMETNET",
-            "addresses": [
-                {
-                    "deliveryPoint": ["Avenue Circulaire 3"],
-                    "city": "Bruxelles",
-                    "postalCode": "1180",
-                    "country": "Belgique",
-                }
-            ],
-            "links": [
-                {
-                    "rel": "about",
-                    "type": "text/html",
-                    "href": "https://www.eumetnet.eu/about-us/",
-                }
-            ],
-            "roles": ["host"],
-        },
-    ],
+    "contact": {
+        "name": "EUMETNET",
+        "url": "https://www.eumetnet.eu/about-us/",
+    },
     "license_info": {
         "name": "CC-BY-4.0",
         "url": "https://creativecommons.org/licenses/by/4.0/",
